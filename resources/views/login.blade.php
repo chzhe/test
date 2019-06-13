cc1c04a18347c5506cda0f83e34ad4b51f36bf59
chzhe <chengzhezhejiji@gmail.com>
2019-06-13 15:44:14 +0800

    adminzhe

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,35 +18,34 @@
             ,form = layui.form;
 
         layer.msg('Hello World');
-    });
-</script>
-<script>
-    //页面层
-    layer.alert('内容')
-    //第三方扩展皮肤
+        //页面层
+        layer.alert('内容')
+        //第三方扩展皮肤
 
-    layer.alert('内容', {
-        icon: 1,
-        skin: 'layer-ext-moon' //该皮肤由layer.seaning.com友情扩展。关于皮肤的扩展规则，去这里查阅
-    })
-    //询问框
+        layer.alert('内容', {
+            icon: 1,
+            skin: 'layer-ext-moon' //该皮肤由layer.seaning.com友情扩展。关于皮肤的扩展规则，去这里查阅
+        })
+        //询问框
 
-    layer.confirm('您是如何看待前端开发？', {
-        btn: ['重要','奇葩'] //按钮
-    }, function(){
-        layer.msg('的确很重要', {icon: 1});
-    }, function(){
-        layer.msg('也可以这样', {
-            time: 20000, //20s后自动关闭
-            btn: ['明白了', '知道了']
+        layer.confirm('您是如何看待前端开发？', {
+            btn: ['重要','奇葩'] //按钮
+        }, function(){
+            layer.msg('的确很重要', {icon: 1});
+        }, function(){
+            layer.msg('也可以这样', {
+                time: 20000, //20s后自动关闭
+                btn: ['明白了', '知道了']
+            });
+        });
+        layer.open({
+            type: 1,
+            skin: 'layui-layer-rim', //加上边框
+            area: ['420px', '240px'], //宽高
+            content: '请注册'
         });
     });
-    layer.open({
-        type: 1,
-        skin: 'layui-layer-rim', //加上边框
-        area: ['420px', '240px'], //宽高
-        content: '请注册'
-    });
 </script>
+
 </body>
 </html>
