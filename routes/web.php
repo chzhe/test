b5c479e50f0e575c949db2cf10f038c870ca3120
chzhe <chengzhezhejiji@gmail.com>
2019-06-17 18:39:51 +0800

    adminzhe

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('login','Admin\UserController@login');//登陆页
-Route::post('login','Admin\UserController@postLogin');//登录接口
-Route::get('login','Admin\UserController@logout');//登出接口
+Route::post('postLogin','Admin\UserController@postLogin');//登录接口
+Route::get('logout','Admin\UserController@logout');//登出接口
 Route::post('register','Admin\UserController@register');//注册接口
 
