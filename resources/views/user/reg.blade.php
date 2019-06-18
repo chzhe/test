10d608e02b24411c7f8e74b3bd69f1e6a16679bf
chzhe <chengzhezhejiji@gmail.com>
2019-06-18 12:49:14 +0800

    adminzhe

--- resources/views/user/reg.blade.php
+++ resources/views/user/reg.blade.php
@@ -1,14 +1,10 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="renderer" content="webkit">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>注册页</title>
+    <title>注册页i</title>
     <link rel="stylesheet" href="../layui/css/layui.css">
-    <link rel="stylesheet" href="../layui/css/layui.css">
-    <link rel="icon" href="../layui/images/face/0.gif">
 </head>
 
 {{--<body>--}}
