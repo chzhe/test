b86cfbb36a4d913e9cb0770a578684af12e9349c
chzhe <chengzhezhejiji@gmail.com>
2019-06-18 12:27:26 +0800

    adminzhe

--- resources/views/user/reg.blade.php
+++ resources/views/user/reg.blade.php
@@ -12,64 +12,65 @@
 </head>
 
 <body>
-{{--<div class="layui-form-item" id="register" style="display: none;">--}}
+<div class="layui-form-item" id="register" style="display: none;">
 
-    {{--<div class="layui-form-item">--}}
-        {{--<div class="layui-form-item ">--}}
-            {{--<h2>TEST</h2>--}}
-            {{--<p>测试系统</p>--}}
-        {{--</div>--}}
-        {{--<div class="layui-form-item">--}}
-            {{--<div class="layui-form-item">--}}
-                {{--<label class="layui-form-item" for="mobile"></label>--}}
-                {{--<input type="text" name="mobile" id="mobile"  required lay-verify="required" placeholder="用户名"--}}
-                       {{--class="layui-input">--}}
-            {{--</div>--}}
-            {{--<div class="layui-form-label">--}}
-                {{--<label class="layui-form-label" for="password"></label>--}}
-                {{--<div class="layui-input-inline">--}}
-                {{--<input type="password" name="password" id="password" required lay-verify="required" placeholder="密码" class="layui-input">--}}
-                {{--</div>--}}
-            {{--</div>--}}
+    <div class="layui-form-item">
+        <div class="layui-form-item ">
+            <h2>TEST</h2>
+            <p>测试系统</p>
+        </div>
+        <div class="layui-form-item">
+            <div class="layui-form-item">
+                <label class="layui-form-item" for="mobile"></label>
+                <input type="text" name="mobile" id="mobile"  required lay-verify="required" placeholder="用户名"
+                       class="layui-input">
+            </div>
+            <div class="layui-form-label">
+                <label class="layui-form-label" for="password"></label>
+                <div class="layui-input-inline">
+                <input type="password" name="password" id="password" required lay-verify="required" placeholder="密码"
+                       class="layui-input">
+                </div>
+            </div>
 
-            {{--<div class="layui-form-item">--}}
-                {{--<button id="reg" class="layui-btn layui-btn-fluid" lay-submit  lay-filter="reg_submit">注册</button>--}}
-            {{--</div>--}}
+            <div class="layui-form-item">
+                <button id="reg" class="layui-btn layui-btn-fluid" lay-submit  lay-filter="reg_submit">注册</button>
+            </div>
 
-        {{--</div>--}}
-    {{--</div>--}}
+        </div>
+    </div>
 
-    {{--<div class="layui-form-item">--}}
+    <div class="layui-form-item">
 
-        {{--<p>© 2019 <a href="chzhe.github.io" target="_blank">MyBlog</a></p>--}}
+        <p>© 2019 <a href="chzhe.github.io" target="_blank">MyBlog</a></p>
 
-    {{--</div>--}}
+    </div>
 
 
 
-{{--</div>--}}
+</div>
 
-{{--<script src="../layui/layui.js"></script>--}}
-{{--<script>--}}
-    {{--layui.use(['form','element', 'layer'],function(){--}}
-        {{--var form = layui.form--}}
-            {{--,element = layui.element--}}
-            {{--,layer = layui.layer--}}
-            {{--,$=layui.jquery;--}}
-        {{--$.ajaxSetup({--}}
-            {{--headers: {--}}
-                {{--'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')--}}
-            {{--}--}}
-        {{--});--}}
-        {{--//监听提交--}}
-        {{--form.on('submit(reg_submit)', function(data){--}}
-            {{--var mobile = data.field.mobile--}}
-                {{--,password = data.field.password;--}}
-            {{--$.ajax({--}}
-                {{--url:"{{url('register')}}",--}}
-                {{--type:'post',--}}
-                {{--dataType:'json',--}}
-                {{--data:{mobile:mobile,password:password},--}}
+<script src="../layui/layui.js"></script>
+<script>
+    layui.use(['form','element', 'layer'],function(){
+        var form = layui.form
+            ,element = layui.element
+            ,layer = layui.layer
+            ,$=layui.jquery;
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        //监听提交
+        form.on('submit(reg_submit)', function(data){
+            var mobile = data.field.mobile
+                ,password = data.field.password;
+            $.ajax({
+                url:"{{url('register')}}",
+                type:'post',
+                dataType:'json',
+                data:{mobile:mobile,password:password},
 
                 {{--success:function(res){--}}
                     {{--if(res.code == 400){--}}
@@ -77,22 +78,22 @@
                     {{--}else{--}}
                         {{--self.location.href="{{url('login')}}";--}}
                     {{--}--}}
-                {{--}--}}
-            {{--});--}}
+                // }
+            });
 
-            {{--return false;--}}
-        {{--});--}}
-    {{--});--}}
+            return false;
+        });
+    });
 
-{{--</script>--}}
-<form  method="post" action="UserController.php">
-    <label for="username">用户名:</label>
-    <input type="text"  name="username" id="username" value="" />
-    <label for="pass">密码:</label>
-    <input type="password"  name="pass" id="pass" value="" />
-    <input type="submit" value="确定"  name="submit" />
-    <input type="reset" value="重置" name="reset" />
-</form>
+</script>
+{{--<form  method="post" action="UserController.php">--}}
+    {{--<label for="username">用户名:</label>--}}
+    {{--<input type="text"  name="username" id="username" value="" />--}}
+    {{--<label for="pass">密码:</label>--}}
+    {{--<input type="password"  name="pass" id="pass" value="" />--}}
+    {{--<input type="submit" value="确定"  name="submit" />--}}
+    {{--<input type="reset" value="重置" name="reset" />--}}
+{{--</form>--}}
 </body>
 
 </html>
