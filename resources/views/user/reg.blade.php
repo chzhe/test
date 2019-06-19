637c886daa9c065a8344b08ba8bc620eb8bf91ab
chzhe <chengzhezhejiji@gmail.com>
2019-06-19 18:10:18 +0800

    adminzhe

--- resources/views/user/reg.blade.php
+++ resources/views/user/reg.blade.php
@@ -30,7 +30,7 @@
 <form class="layui-form">
 
     <div class="layui-form-item">
-        <label class="layui-form-label">手机号</label>
+        <label class="layui-form-label" style="color: #d4d4d4;">手机号</label>
         <div class="layui-input-inline">
             <input type="text" name="mobile" required  lay-verify="required" placeholder="请输入手机号"
                    autocomplete="off" class="layui-input" id ="mo">
