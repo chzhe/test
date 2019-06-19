4e0a0801f15410892b7fd97b9b69f6368497c150
chzhe <chengzhezhejiji@gmail.com>
2019-06-19 17:42:21 +0800

    adminzhe

--- resources/views/user/reg.blade.php
+++ resources/views/user/reg.blade.php
@@ -21,7 +21,7 @@
 <!-- 你的HTML代码 -->
 
 <h1 class="txtCenter" style="color:green">注册</h1>
-<div style="width: 650px; position: relative; left: 40%;">
+<div style="width: 650px; position: relative; left: 45%;">
 <form class="layui-form">
 
     <div class="layui-form-item">
@@ -41,7 +41,7 @@
         <div class="layui-form-item">
             <div class="layui-input-block">
                 <button class="layui-btn" lay-submit lay-filter="formDemo">立即提交</button>
-                <button type="reset" class="layui-btn layui-btn-primary"  style="background-color: #0d3625">重置</button>
+                <button type="reset" class="layui-btn layui-btn-primary"  style="background-color:darkgreen">重置</button>
             </div>
         </div>
     </div>
