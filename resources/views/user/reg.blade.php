b81174c25c1437b0caca1ad533e1b2bd2e98e0c8
chzhe <chengzhezhejiji@gmail.com>
2019-06-19 17:49:20 +0800

    adminzhe

--- resources/views/user/reg.blade.php
+++ resources/views/user/reg.blade.php
@@ -40,8 +40,8 @@
 
         <div class="layui-form-item">
             <div class="layui-input-block">
-                <button class="layui-btn" lay-submit lay-filter="formDemo" style="background-color:green">立即提交</button>
-                <button type="reset" class="layui-btn layui-btn-primary" style="color:white" style="background-color:green">重置</button>
+                <button class="layui-btn" lay-submit lay-filter="formDemo">立即提交</button>
+                <button type="reset" class="layui-btn layui-btn-primary">重置</button>
             </div>
         </div>
     </div>
