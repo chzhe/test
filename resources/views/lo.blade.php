00d2bac24c87269805e437066e8d9c2968eb5719
chzhe <chengzhezhejiji@gmail.com>
2019-06-20 20:00:10 +0800

    adminzhe

--- resources/views/lo.blade.php
+++ resources/views/lo.blade.php
@@ -77,8 +77,10 @@
                     success : function (response) {
                         if(response.code==200){
                             layer.msg ('登录成功');
-                            self.location.reload(document.getElementById("btn2").innerText = pram.mobile+"已登录"
-                        );
+                            self.location.reload();
+                            if(self.location.reload()){
+                                document.getElementById("btn2").innerText = pram.mobile+"已登录";
+                            }
 
                             {{--self.location.href="{{url('main')}}";--}}
                         }else{
