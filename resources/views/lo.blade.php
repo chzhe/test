c5913227ba190fc6363ee10f9b1bb7c207cc7815
chzhe <chengzhezhejiji@gmail.com>
2019-06-20 19:46:38 +0800

    adminzhe

--- resources/views/lo.blade.php
+++ resources/views/lo.blade.php
@@ -78,8 +78,8 @@
                         if(response.code==200){
                             layer.msg ('登录成功');
                             self.location.reload();
-
-                            document.getElementById("btn2").innerText = pram.mobile+"已登录";
+                           if(response.code==200){
+                            document.getElementById("btn2").innerText = pram.mobile+"已登录";}
                             {{--self.location.href="{{url('main')}}";--}}
                         }else{
                             layer.msg(response.data)
