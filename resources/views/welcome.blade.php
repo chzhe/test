6a60f34edea171bce136014943a230a9ded20cce
chzhe <chengzhezhejiji@gmail.com>
2019-06-20 15:34:27 +0800

    adminzhe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,7 +67,7 @@
 <body>
 <button id ="btn1" class="layui-btn" onclick="click()" style="color: #20a53a">请先注册</button>
 <div id = 'test' style="display: none">
-    <p class="txtCenter" style="color:green">注册</p>
+    <p class="txtCenter" style="align-content: center" style="color:green">注册</p>
     <div style="width: 650px; position: relative; left: 43%;">
         <form class="layui-form">
 
