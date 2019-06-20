809fde2bc8773fb20c2ab1e4290f205949bd9948
chzhe <chengzhezhejiji@gmail.com>
2019-06-20 12:46:03 +0800

    ad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,7 @@
     </style>
 </head>
 <body>
+<div class="layui-btn">请登录</div>
 <div class="flex-center position-ref full-height">
     @if (Route::has('login'))
         <div class="top-right links">
