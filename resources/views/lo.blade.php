c14fe424061ef541a3bceb6882600db908b76c36
chzhe <chengzhezhejiji@gmail.com>
2019-06-20 20:28:50 +0800

    adminzhe

--- resources/views/lo.blade.php
+++ resources/views/lo.blade.php
@@ -77,11 +77,13 @@
                     success : function (response) {
                         if(response.code==200){
                             layer.msg ('登录成功');
-                            var r = self.location.reload();
-                             r =function () {
-                                     self.document.getElementById("btn2").innerText = "已登录";
-                                     self.document.getElementById("btn2").onclick = "";
-                             };
+                         self.location.reload(function () {
+                             self.document.getElementById("btn2").innerText = "已登录";
+                             self.document.getElementById("btn2").onclick = "";
+                         });
+
+
+
                             //     self.document.getElementById("btn2").innerText = "已登录";
                             //     self.document.getElementById("btn2").onclick = "";
 
