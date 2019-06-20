202c48cc8706d5edfd0b4a7e12fd0a42c67c0bf5
chzhe <chengzhezhejiji@gmail.com>
2019-06-20 18:26:31 +0800

    adminzhe

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,42 +16,6 @@
 <body>
 <h1 style="color: #411c0e;text-align: center">That's All.</h1>
 
-<ul class="layui-nav" lay-filter="">
-    <li class="layui-nav-item  layui-this"><a href="/why123/user/saveUser" style="font-size:20px;">首页</a></li>
-    <li class="layui-nav-item "><a href="/why123/news/forwardNews" style="font-size:20px;">新闻</a></li>
-    <li class="layui-nav-item "><a href="#" style="font-size:20px;">家庭活动</a></li>
-    <li class="layui-nav-item ">
-        <a href="javascript:;" style="font-size:20px;">家庭相册</a>
-        <dl class="layui-nav-child"> <!-- 二级菜单 -->
-            <dd><a href="" style="font-size:15px;">个人相册</a></dd>
-            <dd><a href="" style="font-size:15px;">他人相册</a></dd>
-            <dd><a href="" style="font-size:15px;">热门相册</a></dd>
-        </dl>
-    </li>
-    <li class="layui-nav-item"><a href="#" style="font-size:20px;">意见反馈</a></li>
-    <li class="nav-user">
-        <% String userName=(String)session.getAttribute("userName");
-        if(userName==null){ %>
-        <div>
-            <a href="/why123/login/userLogin" class="nav-denglu">登录</a>
-        </div>
-        <%}else{%>
-        <div class="user-msg">
-            <ul class="layui-nav nav-userlogin">
-                <li class="layui-nav-item">
-                    <a href="" style="font-size:18px;"><img src="plug-in/images/people/renwu.jpg" class="layui-nav-img" style="width:40px;height:40px;">${sessionScope.userName}</a>
-                    <dl class="layui-nav-child">
-                        <dd><a href="javascript:;" style="font-size:18px;">修改信息</a></dd>
-                        <dd><a href="javascript:;" style="font-size:18px;">个人管理</a></dd>
-                        <dd><a href="javascript:;" style="font-size:18px;">注销</a></dd>
-                    </dl>
-                </li>
-            </ul>
-        </div>
-        <%} %>
-    </li>
-</ul>
-
 
 <script src="../layui/layui.js"></script>
 <script>
